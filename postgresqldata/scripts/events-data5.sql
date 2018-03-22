fb58133d74c733e571a813863965e7d9b0a2e532
mitrao <pedropires_23@yahoo.com>
2018-03-22 15:39:08 +0000

    postgres docker image with initial events data

---
+++ postgresqldata/scripts/events-data5.sql
@@ -0,0 +1,6 @@
+INSERT INTO TRAFFIC_TYPE (TRAFFIC_TYPE_ID, SHDESC, DESCRIPTION) VALUES (0,'Other', 'Other types of traffic');
+INSERT INTO TRAFFIC_TYPE (TRAFFIC_TYPE_ID, SHDESC, DESCRIPTION) VALUES (1,'On-Net', 'Communications inside of same operator');
+INSERT INTO TRAFFIC_TYPE (TRAFFIC_TYPE_ID, SHDESC, DESCRIPTION) VALUES (2,'Off-Net', 'Communications between operators in same country');
+INSERT INTO TRAFFIC_TYPE (TRAFFIC_TYPE_ID, SHDESC, DESCRIPTION) VALUES (3,'International', 'Comnunications from/to others countries');
+INSERT INTO TRAFFIC_TYPE (TRAFFIC_TYPE_ID, SHDESC, DESCRIPTION) VALUES (4,'Roaming', 'Communications in Roaming');
+INSERT INTO TRAFFIC_TYPE (TRAFFIC_TYPE_ID, SHDESC, DESCRIPTION) VALUES (5,'Other', 'Other types of traffic');
